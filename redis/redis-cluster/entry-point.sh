033a3b7d8ad2017e1363acefbfd046da3fd2f997
shunliz <zsl6658@126.com>
2017-08-11 14:35:51 +0800

    update redis cluster

--- redis/redis-cluster/entry-point.sh
+++ redis/redis-cluster/entry-point.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-/usr/local/redis1/bin/redis-server /usr/local/redis1/etc/redis.conf
-/usr/local/redis2/bin/redis-server /usr/local/redis2/etc/redis.conf
-/usr/local/redis3/bin/redis-server /usr/local/redis3/etc/redis.conf
-/usr/local/redis4/bin/redis-server /usr/local/redis4/etc/redis.conf
-/usr/local/redis5/bin/redis-server /usr/local/redis5/etc/redis.conf
-/usr/local/redis6/bin/redis-server /usr/local/redis6/etc/redis.conf
-
+cd /usr/local/redis1
+bin/redis-server etc/redis.conf
+cd /usr/local/redis2
+bin/redis-server etc/redis.conf
+cd /usr/local/redis3
+bin/redis-server etc/redis.conf
+cd /usr/local/redis4
+bin/redis-server etc/redis.conf
+cd /usr/local/redis5
+bin/redis-server etc/redis.conf
+cd /usr/local/redis6
+bin/redis-server etc/redis.conf
 
 cd /usr/local/
-./redis-trib.rb create –replicas 1 127.0.0.1:7001 127.0.0.1:7002 127.0.0.1:7003 127.0.0.1:7004 127.0.0.1:7005 127.0.0.1:7006
-
+echo "yes" | ./redis-trib.rb create –replicas 1 127.0.0.1:7001 127.0.0.1:7002 127.0.0.1:7003 127.0.0.1:7004 127.0.0.1:7005 127.0.0.1:7006
+/bin/sh
